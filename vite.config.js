bbfc27ed32606477ee6dd8e6a165234c6166bce5
Devdatt Gautam <devg26092001@gmail.com>
2023-11-28 15:06:29 +0530

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,12 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Threads-Clone-Frontend/",
+  base: "/Threads-Clone-Frontend",
   server: {
     port: 3000,
     //get rid of cors errors
     proxy: {
       "/api": "https://threads-clone-8hjb.onrender.com",
-      secure: false,
     },
   },
 });
